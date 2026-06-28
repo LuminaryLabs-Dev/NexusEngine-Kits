0bed9880acafd148abd9d76f44f0bbf3e414161b
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-28 09:19:34 -0400

    Export modular installer helpers

--- installer/index.js
+++ installer/index.js
@@ -30,3 +30,57 @@ export {
 export {
   createNexusRealtimeKitInstaller
 } from "./kit-installer.js";
+
+export {
+  createNexusRealtimeKitInstaller as createKitInstaller
+} from "./create-kit-installer.js";
+
+export {
+  installKit
+} from "./install-kit.js";
+
+export {
+  installDomain,
+  installDomainKits
+} from "./install-domain.js";
+
+export {
+  installBundle,
+  installBundleKits
+} from "./install-bundle.js";
+
+export {
+  installAll,
+  installAllKits
+} from "./install-all.js";
+
+export {
+  resolveKit
+} from "./resolve-kit.js";
+
+export {
+  resolveDomain
+} from "./resolve-domain.js";
+
+export {
+  resolveCdn
+} from "./resolve-cdn.js";
+
+export {
+  validateKitManifest,
+  assertValidKitManifest
+} from "./validate-kit-manifest.js";
+
+export {
+  validateDomainManifest,
+  assertValidDomainManifest
+} from "./validate-domain-manifest.js";
+
+export {
+  validateInstallPlan
+} from "./validate-install-plan.js";
+
+export {
+  createInstallReport,
+  mergeInstallReports
+} from "./install-report.js";
