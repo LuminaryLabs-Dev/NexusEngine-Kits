73d88f8961c1e7304976a3e219acaa62cdb8bbca
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-28 04:15:53 -0400

    Add root kit catalog entrypoint

---
+++ src/index.js
@@ -0,0 +1,25 @@
+export * from "../installer/index.js";
+
+export {
+  createAllNexusRealtimeKits
+} from "../bundles/all.js";
+
+export {
+  createDefaultGameStackKits
+} from "../bundles/default-game-stack.js";
+
+export {
+  createAerialGameStackKits
+} from "../bundles/aerial-game-stack.js";
+
+export {
+  createRpgGameStackKits
+} from "../bundles/rpg-game-stack.js";
+
+export {
+  createDefenseGameStackKits
+} from "../bundles/defense-game-stack.js";
+
+export {
+  createXrAuthoringStackKits
+} from "../bundles/xr-authoring-stack.js";
