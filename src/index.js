fd9dfae10b1cbf3d753f96b60a138c42ed7cde76
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-28 04:25:49 -0400

    Export first-wave kit factories from root

--- src/index.js
+++ src/index.js
@@ -23,3 +23,43 @@ export {
 export {
   createXrAuthoringStackKits
 } from "../bundles/xr-authoring-stack.js";
+
+export {
+  createCompletionLedgerKit
+} from "../kits/spatial/completion-ledger-kit/index.js";
+
+export {
+  createSpatialInteractionKit
+} from "../kits/spatial/spatial-interaction-kit/index.js";
+
+export {
+  createObjectiveBridgeKit
+} from "../kits/progression/objective-bridge-kit/index.js";
+
+export {
+  createLockGroupKit
+} from "../kits/progression/lock-group-kit/index.js";
+
+export {
+  createDamageHealthKit
+} from "../kits/hazard-combat/damage-health-kit/index.js";
+
+export {
+  createResourceNodeKit
+} from "../kits/economy-resources/resource-node-kit/index.js";
+
+export {
+  createBuildPlacementKit
+} from "../kits/building/build-placement-kit/index.js";
+
+export {
+  createStructureRuntimeKit
+} from "../kits/building/structure-runtime-kit/index.js";
+
+export {
+  createAssetDescriptorKit
+} from "../kits/render-descriptors/asset-descriptor-kit/index.js";
+
+export {
+  createDiegeticFeedbackSignalKit
+} from "../kits/camera-feedback/diegetic-feedback-signal-kit/index.js";
